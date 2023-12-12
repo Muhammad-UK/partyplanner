d474d1507c806c0873da71af019648f44bf45223
Muhammad-UK <muhammadkhan3655@gmail.com>
2023-12-12 11:44:39 -0600

    added async function

--- app.js
+++ app.js
@@ -5,14 +5,30 @@ let parties = [];
 const render = () => {
   spanCount.innerHTML = parties.length;
   const html = parties
+    //wanted to get the datetime attribute to work but does not seem like it will unless I use some Date object and a tolocalestring method which we have not covered in class
     .map((party) => {
       return `
-            <h4>${party.name}</h4>
-            <article>${party.description}</article>
-            <time datetime="${party.date}">
-            <p>${party.location}</p>
+            <h4>Name: ${party.name}</h4>
+            <article>Description: ${party.description}</article>
+            <p>Date and Time: <time datetime="${party.date}">${party.date}</time></p> 
+            <p>Location: ${party.location}</p>
         `;
     })
     .join("");
   sectionParties.innerHTML = html;
 };
+
+const fetchParties = async () => {
+  try {
+    const response = await fetch(
+      "https://fsa-crud-2aa9294fe819.herokuapp.com/api/2309_ftb_et_web_am/events"
+    );
+    const json = await response.json();
+    parties = json.data;
+    render();
+  } catch (ex) {
+    console.log(ex);
+  }
+};
+
+fetchParties();
